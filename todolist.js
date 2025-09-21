71d4d44d933600899ef405e12db49d7fe785a289
Sharali-25 <Sharaliarkansas@gmail.com>
2025-09-21 10:12:30 -0500

    Add files via upload

---
+++ todolist.js
@@ -0,0 +1,16 @@
+var myForm=document.getElementById("myForm");
+var myInput=document.getElementById("myInput");
+var myItem=document.getElementById("myItem");
+myForm.addEventListener("submit", function(event){
+  event.preventDefault()
+  createItem(myInput.value)
+})
+function createItem(inputItems){
+  var items=`<li>${inputItems} <button onclick="deleteElement(this)">Delete</button></li>`
+  myItem.insertAdjacentHTML("beforeEnd",items)
+  myInput.value=""
+  myInput.focus()
+}
+function deleteElement(ElementToDelete){
+  ElementToDelete.parentElement.remove()
+}
